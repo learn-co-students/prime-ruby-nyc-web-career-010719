a1a5793cf67b21f139642259b48678393d2130af
sarahshin <sarahsujin.shin@gmail.com>
2018-12-27 00:12:11 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(int)
+  if(int <= 1)
+    return false
+  end
+  div_Array = Array (2.. (int-1))
+  div_Array.each do |i|
+    if(int % i == 0)
+      return false
+    end
+  end
+  return true
+end
